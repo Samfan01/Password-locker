8d27bac118dc83b873e63c485747b71402541c49
samfan01 <dsamfan@gmail.com>
2021-02-21 23:31:30 +0300

    created delete method to delete users from the user_list

--- user.py
+++ user.py
@@ -17,5 +17,9 @@ class User:
         save_user method saves user objects into the user_list
         '''
         User.user_list.append(self)
-        
+    def delete_user(self):
+        '''
+        delete_user method that deletes a saved user
+        '''
+        User.user_list.remove(self)
     
