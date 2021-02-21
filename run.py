7b39202c8795689684bf94e7a54ec094be4275b7
samfan01 <dsamfan@gmail.com>
2021-02-22 01:47:29 +0300

    created the function for checking if a user exists

--- run.py
+++ run.py
@@ -7,16 +7,28 @@ def create_user(username,password):
     '''
     new_user = User(username,password)
     return new_user
-def save_user():
+def save_user(user):
     '''
     Function to save user
     '''
     user.save_user()
-def delete_user():
+def delete_user(user):
     '''
     Function to delete a user
     '''
     user.delete_user()
+def find_user(username)
+    '''
+    Function that finds a user by username and returns 
+    the user
+    '''
+    return User.find_by_username(username)
+def check_existing_users(username):
+    '''
+    Function that check if a user exists with that username
+    '''
+    return user.user_exist(username)
+
 
 
 
