21ffca7b978bbd732fe85531dcad0791d71725f0
samfan01 <dsamfan@gmail.com>
2021-02-22 02:00:24 +0300

    created the credentials test case file

---
+++ credential_test.py
@@ -0,0 +1,12 @@
+import unittest
+import user
+from Credentials import Credential
+
+
+class TestUser(unittest.TestCase):
+    '''
+    Test case class for credential class behaviours
+    Args:
+        unittest.TestCase: TestCase class that will
+        help in creatin test cases
+    '''
--- credentials.py
+++ credentials.py
@@ -2,3 +2,13 @@ class Credential:
     '''
     class to  create new instances of account credentials
     '''
+    Credential_list = []
+    
+    def __init__(self,account_name,account_password):
+        '''
+        passed three arguments to represent instances of our variables
+        '''
+        self.account_name = account_name
+        self.account_password = account_password
+        
+    
