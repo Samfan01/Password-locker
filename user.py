9de9e17daaf3118efa414c3ef6bac94e03e92f97
samfan01 <dsamfan@gmail.com>
2021-02-21 22:06:24 +0300

    created a class user

---
+++ user.py
@@ -0,0 +1,18 @@
+class User:
+    '''Class that generates new instances of users.
+    '''
+    
+    User_list = []
+    
+def __init__(self,username,pasword):
+    '''
+    __init__ method that helps ud define properties for our objects.
+    
+    Args:
+        username:New user username.
+        password:New user password.
+        
+    '''
+    
+    self.username = username
+    self.password = password
