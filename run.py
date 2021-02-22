410dad6681fb05640c4447c6961cc369ab8a9ca2
samfan01 <dsamfan@gmail.com>
2021-02-22 03:10:32 +0300

    added the credential functions on run.py file

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3.6
 from user import User
+from credentials import Credential
+import random
+import string
+
 
 def create_user(username,password):
     '''
@@ -23,19 +27,58 @@ def find_user(username)
     the user
     '''
     return User.find_by_username(username)
-def check_existing_users(username):
+def check_existing_user(username):
     '''
     Function that check if a user exists with that username
     '''
     return user.user_exist(username)
-def display_users():
+def display_user():
     '''
     Function that returns all the saved users
     '''
-    return User.display_users()
+    return User.display_user()
+def create_credential(account_name,account_password)
+    '''
+    Function to save new account
+    '''
+    new_account = Credential(account_name,account_password)
+    return new_account
+def save_credential(credential):
+    '''
+    for saving credentials
+    '''
+    credential.save_credential
+    
+def password_generetor():
+    '''
+    function to generate password for the user
+    '''
+    P = string.ascii_uppercase + string.ascii_lowercase + string.digits
+    print('Enter the length of your Password ')
+    p_length = int(input())
+    password = ''.join(random.choices(P, k=pass_length))
+    
+    return password    
+def display_credential(credential):
+    '''
+    function to display credentials
+    '''
+    return Credential.display_credential()
+def delete_credential(credential):
+    '''
+    function to delete a credential
+    '''
+    Credential.delete_credential()
+def check_existing_credential(account_name):
+    '''
+    to check if a credential exists
+    '''
+    return Credential.credential_exists(account_name)
+def find_credential(account_name):
+    '''
+    to find a specific saved credential
+    '''
+    return Credential.find_by_account_name(account_name)
 
-
-
-
-
-print('**'*15+'PASSWORD-LOCKER'+'**'*15)
+def main():
+    print('**'*15+'PASSWORD-LOCKER'+'**'*15)
